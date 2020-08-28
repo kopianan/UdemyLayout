308924ad21279e6a50e658c87071a529ec1d4533
ctrlaltdie <ananalfredcarlos@gmail.com>
2020-08-28 19:06:37 +0700

    first commit

---
+++ lib/presentation/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:new_apps/presentation/profile/profile_page.dart';
+
+import 'home/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
